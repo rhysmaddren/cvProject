e8ea609bc1041dde04abbe9a3627bf99bbcafb26
rhysmaddren <rhysmaddren@hotmail.co.uk>
2020-08-11 14:59:52 +0100

    Added nav skeleton and icon

--- scripts.js
+++ scripts.js
@@ -1,17 +1,27 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the header
 var header = document.getElementById("myHeader");
+var section = document.getElementById("about");
+var menuicon = document.getElementById("menuicon");
+var sticky = header.offsetTop; // get offset from top
 
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
+// ensures that the menu icon is not visible on page load
+window.onload = function() {
+  menuicon.classList.remove("icon");
+}
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+// When the user scrolls the page, the headerFunction is called
+window.onscroll = function() {headerFunction()};
+
+// Adds and removes sticky class - also implements fix for jerky movement.
+// also added menuicon on scroll.
+function headerFunction() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
+    section.classList.add("stickyfix");
+    menuicon.classList.add("icon");
   } else {
     header.classList.remove("sticky");
+    section.classList.remove("stickyfix");
+    menuicon.classList.remove("icon");
   }
 }
+
